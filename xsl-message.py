854069823d724af0e1f01469339f2ca9ff3d9670
Pablo Conca <pabloconcagran@gmail.com>
2015-05-27 16:16:01 +0200

    Show the message on the cursor position

--- xsl-message.py
+++ xsl-message.py
@@ -2,4 +2,4 @@ import sublime, sublime_plugin
 
 class XslMessageCommand(sublime_plugin.TextCommand):
 	def run(self, edit):
-		self.view.insert(edit, 0, '<xsl:message>variable<xsl:value-of select="$variable"/></xsl:message>')
+		self.view.insert(edit, self.view.sel()[0].begin(), '<xsl:message>variable<xsl:value-of select="$variable"/></xsl:message>')
